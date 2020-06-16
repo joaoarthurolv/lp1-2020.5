2b1cc43434ea00794800af8270df5204f97b57f3
João Arthur Oliveira Barbosa <joaoarthur98@ufrn.edu.br>
2020-06-16 09:31:21 -0300

    Ajuste na verificação

--- aula02.cpp
+++ aula02.cpp
@@ -2,22 +2,24 @@
 #include <string>
 
 int main(int argc, char *argv[]){
+    std::string arg = "add";
 
     if(argc == 1){
         std::cout << "Uso: " << argv[0] << " add <mensagem>" << std::endl;
         return -1;
     }
 
-    if(argc == 2){
+    if(argc == 2 && arg.compare(argv[1]) == 0){
         std::string mensagem;
 
+        std::cout << arg.compare(argv[1]);
         std::cout << "Informe a mensagem a ser impressa: " << std::endl;
         std::cin >> mensagem;
 
         std::cout << mensagem << std::endl; 
     }
 
-    if(argc == 3){
+    if(argc == 3 && arg.compare(argv[1]) == 0){
         std::cout << argv[2] << std::endl;
     }
 
