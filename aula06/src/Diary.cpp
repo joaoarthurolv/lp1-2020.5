282f060ac800a03e029d7e96eec11168da9370f2
João Arthur Oliveira Barbosa <joaoarthur98@ufrn.edu.br>
2020-06-23 10:50:47 -0300

    Aula 06

---
+++ aula06/src/Diary.cpp
@@ -0,0 +1,16 @@
+#include "../include/Diary.h"
+
+#include <iostream>
+
+Diary::Diary(const std::string& filename) : filename(filename)
+{
+    
+}
+
+void Diary::add(const std::string& message)
+{
+}
+
+void Diary::write()
+{
+}
